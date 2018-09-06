5616ce8258fb65cc9a44bcd6a8f20af5a2bd11b5
Marcelo <marcelo.agudelo@gmail.com>
2018-09-06 13:58:10 -0400

    Update todoListRoutes.js

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -12,4 +12,9 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+
+
+  app.route('/json')
+    .get(todoList.show_json);
+
 };
